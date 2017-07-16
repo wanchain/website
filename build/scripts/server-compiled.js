0fcd1476ded6519bef31bdc39d06459706b3337d
astro913 <astro913@163.com>
2017-07-16 21:45:18 +0800

    add https 1.3

--- build/scripts/server-compiled.js
+++ build/scripts/server-compiled.js
@@ -3,9 +3,7 @@
 var logger = require('../lib/logger');
 
 logger.info('Starting server...');
-var app = require('../../server/mainServer');
-app.listen(80);
-app.listen(443, function () {
+require('../../server/mainServer').listen(443, function () {
   logger.success('Server is running at http://localhost:443');
 });
 
--- build/scripts/server.js
+++ build/scripts/server.js
@@ -1,8 +1,6 @@
 const logger = require('../lib/logger');
 
 logger.info('Starting server...');
-var app = require('../../server/mainServer');
-app.listen(80);
-app.listen(443, () => {
+require('../../server/mainServer').listen(443, () => {
   logger.success('Server is running at http://localhost:443')
 });
