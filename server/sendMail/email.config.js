ff73a9474d50411e2a0e6d179e926e0dfbf1180a
astro913 <astro913@163.com>
2017-07-24 11:54:37 +0800

    add sendmail v1.1

--- server/sendMail/email.config.js
+++ server/sendMail/email.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     email: {
-        service: 'QQex',
-        user: 'info@wanchain.org',
-        pass: 'gFSeVU9ad2kSCY3T',
+        service: '**',
+        user: '****',
+        pass: '********',
     }
 };
--- server/sendMail/test.js
+++ server/sendMail/test.js
@@ -1,4 +1,4 @@
 
 const sendMail = require('./mail');
 
-sendMail('1274217462@qq.com','这是测试邮件', 'Hi Astro,这是一封测试邮件');
+sendMail('you mail','title', 'content');
