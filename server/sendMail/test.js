572e46df9d770f248133e8a9bb359a64f60d89c6
astro913 <astro913@163.com>
2017-07-24 14:53:06 +0800

    add sendmail v1.3

--- server/sendMail/test.js
+++ server/sendMail/test.js
@@ -3,4 +3,4 @@ const sendMail = require('./mail');
 
 const content = require('./content');
 
-sendMail('1274217462@qq.com, sunquake@126.com','感谢您订阅Wanchain,获得有关项目的最新动态信息', content);
+sendMail('your mail','title', content);
