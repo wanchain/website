b43a9283da15b5dc56c34d3eafb3721bb8f4f8c2
astro913 <astro913@163.com>
2017-07-25 16:16:24 +0800

    del useless files

--- server/sendMail/email.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    email: {
-        service: '**',
-        user: '******',
-        pass: '******',
-    }
-};
--- server/sendMail/test.js
+++
@@ -1,6 +0,0 @@
-
-const sendMail = require('./mail');
-
-const content = require('./content');
-
-sendMail('your mail','my title', content);
