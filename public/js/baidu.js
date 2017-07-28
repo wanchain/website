5b3a3cf0ae8651258913b41e253d9bcc21accde0
astro913 <astro913@163.com>
2017-07-28 14:47:32 +0800

    add baidu statistics

---
+++ public/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?746e13ea3226c1769f5774ab32c990e2";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
