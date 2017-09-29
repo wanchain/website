4bf0c061e4d3f4db31eb798dcde327f5396ae7cb
astro913 <astro913@163.com>
2017-09-29 15:11:32 +0800

    update tokensale page

--- src/components/VideoWarn/index.js
+++ src/components/VideoWarn/index.js
@@ -30,8 +30,8 @@ export default class VideoWarningModal extends Component {
               <h4>Address Check</h4>
               <hr/>
               <span className="glyphicon glyphicon-remove" id={styles['astro-warning-gly']} onClick={this.onCloseFunc.bind(this)}></span>
-              <video style={{width: '100%'}} controls="controls" >
-                <source src="/upload/video.mp4" type='"video/mp4"; codecs="avc1.42E01E, mp4a.40.2"'></source>
+              <video style={{width: '100%'}} controls="controls" autoBuffer="autobuffer">
+                <source src="/upload/video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></source>
                 your browser does not support the video tag
               </video>
               <div className={styles['astro-warning-modal-button']}>
