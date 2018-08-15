9146c35e7cf51957745de9dc08358805bda19248
yi-DaLao <26593265@qq.com>
2018-08-15 10:57:28 +0800

    update events

--- src/containers/Events/Events.js
+++ src/containers/Events/Events.js
@@ -19,69 +19,24 @@ class Events extends Component {
             <table border="0" className={styles.eventsTable}>
               <tbody>
                 <tr>
-                  <th width="40%">Event Name</th>
-                  <th width="40%">Dates</th>
-                  <th width="20%">Location</th>
+                  <th width="50%">Event Name</th>
+                  <th width="25%">Dates</th>
+                  <th width="25%">Location</th>
                 </tr>
                 <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/cryptocurrency-blockchain-20-wanchain-and-partners-coming-to-jakarta-tickets-46807312933">Wanchain Meetup</a></td>
-                  <td>05/07/2018  -  05/07/2018</td>
-                  <td>Jakarta, Indonesia</td>
+                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-wanlabs-meetup-beijng-tickets-48685254908"> Wanchain meetup series</a></td>
+                  <td>8/18</td>
+                  <td>Beijng</td>
                 </tr>
                 <tr>
-                  <td><a target="_blank" href="https://hacksummit.org/hackathon">Hack.Summit Virtual Hackathon</a></td>
-                  <td>06/07/2018  -  08/07/2018</td>
-                  <td>Virtual</td>
+                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-wanlabs-meetup-shanghai-tickets-49027519631"> Wanchain meetup series</a></td>
+                  <td>9/8</td>
+                  <td>Shanghai</td>
                 </tr>
                 <tr>
-                  <td><a target="_blank" href="https://www.fintechweek.com/?gclid=CjwKCAjw9qfZBRA5EiwAiq0Abf9KSPiPlZvPTFUSg5OH-Ha1I4jTJNXo6AdRN0KaH9NjEfeSfRiUohoCWMwQAvD_BwE">London Fintech week</a></td>
-                  <td>06/07/2018  -  13/07/2018</td>
-                  <td>London, United Kindom</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/cryptocurrency-blockchain-30-wanchain-and-partners-asia-tour-2018-feat-jack-lu-tickets-46653927152?aff=ebdssbdestsearch">Wanchain Meetup</a></td>
-                  <td>07/07/2018  -  07/07/2018</td>
-                  <td>Kuala Lumpur, Malaysia</td>
-                </tr>
-                <tr>
-                  <td>Bitrue & Wanchain Meetup</td>
-                  <td>10/07/2018 -  11/07/2018</td>
-                  <td>Taipei, Taiwan</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/cryptocurrency-blockchain-20wanchain-and-partners-asia-tour-2018seoul-tickets-46877334369">Wanchain Meetup</a></td>
-                  <td>14/07/2018 -  14/07/2018</td>
-                  <td>Seoul, South Korea</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-and-wanlabs-european-tour-london-tickets-47642007527">Wanchain European Roadshow London</a></td>
-                  <td>19/07/2018 -  19/07/2018</td>
-                  <td>London, United Kindom</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-and-wanlabs-european-tour-amsterdam-tickets-47655252142">Wanchain European Roadshow Amsterdam</a></td>
-                  <td>23/07/2018 -  23/07/2018</td>
-                  <td>Amsterdam, Netherlands</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-and-wanlabs-european-tour-berlin-tickets-47655312322">Wanchain European Roadshow Zurich</a></td>
-                  <td>25/07/2018 -  25/07/2018</td>
-                  <td>Zurich, Switzerland</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-and-wanlabs-european-tour-berlin-tickets-47655312322">Wanchain European Roadshow Berlin</a></td>
-                  <td>27/07/2018 -  27/07/2018</td>
-                  <td>Berlin Germany</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="https://www.eventbrite.co.uk/e/blockchain-summit-singapore-tickets-42277553300">Blockchain Summit</a></td>
-                  <td>28/07/2018 -  28/07/2018</td>
-                  <td>Singapore</td>
-                </tr>
-                <tr>
-                  <td><a target="_blank" href="http://www.xblockchain.tech/">XBlockchain Summit</a></td>
-                  <td>09/10/2018  -  10/10/2018</td>
-                  <td>Bali, Indonesia</td>
+                  <td><a target="_blank" href="https://www.eventbrite.com/e/wanchain-20-wanlabs-meetup-shenzhen-tickets-49027884723"> Wanchain meetup series</a></td>
+                  <td>9/16</td>
+                  <td>Shenzhen</td>
                 </tr>
               </tbody>
             </table>
