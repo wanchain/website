08aa9cbcd9771ae0ab3f5f6417a1e1c153219f42
liusheng112 <361707502@qq.com>
2018-11-05 10:30:30 +0800

    updated team page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -24,22 +24,21 @@ class About extends Component {
       const blog2 = require('./image/team.png');
       const styles = require('./About.scss');
       const isLinkedIn = 'https://www.linkedin.com/';
-      const other = require('./image/other.png');
       const In = require('./image/in.png');
       const team = teamDataEn;
       const advisory = dataAdvisoryEn;
 
       return (
             <div className={styles.aboutDiv}>
-                <div className={styles.aboutHeader}>
+                {/* <div className={styles.aboutHeader}>
                     <img className={styles.aboutHeaderImg1} src={blog1} />
                     <img className={styles.aboutHeaderImg2} src={blog2} />
                     <h2>{language === 'zn' ? '项目团队' : 'Wanchain'}</h2>
-                </div>
+                </div> */}
 
                 <div className={styles.aboutBody + ' container'}>
                     <div className={styles.aboutTitle}>
-                        <h2>{language === 'zn' ? '核心团队' : 'Wanchain Team'}</h2>
+                        <h1>{language === 'zn' ? '核心团队' : 'Wanchain Team'}</h1>
                     </div>
 
                     <div className={styles.aboutTeam + ' col-lg-12'}>
@@ -48,9 +47,9 @@ class About extends Component {
                           return (
                               <div className={styles.aboutTitleDiv + ' col-lg-4'} key={index}>
                                   <img className={styles.title} src={value.img}></img>
-                                  <h4>{value.title}</h4> 
-                                  <h4><small>{value.position}</small></h4> 
-                                  {value.url ? <a href={value.url} target="_blank">{value.url.startsWith(isLinkedIn) ? <img src={In} className={styles.link}></img> : <img src={other} className={styles.link}></img>}</a> : '' }
+                                  <h4>{value.title}</h4>
+                                  <h4><small>{value.position}</small></h4>
+                                  {value.url ? <a href={value.url} target="_blank">{value.url.startsWith(isLinkedIn) ? <em className={styles.linkIn}></em> : <em className={styles.linkOther}></em>}</a> : '' }
                               </div>
                           );
                         })
@@ -58,7 +57,7 @@ class About extends Component {
                     </div>
 
                     <div className={styles.aboutTitle}>
-                        <h2>{language === 'zn' ? '顾问团队' : 'Advisory Group'}</h2>
+                        <h1>{language === 'zn' ? '顾问团队' : 'Advisory Group'}</h1>
                     </div>
 
                     <div className="col-lg-12">
@@ -67,9 +66,9 @@ class About extends Component {
                           return (
                               <div className={styles.aboutTitleDiv2 + ' col-lg-4'} key={index}>
                                   <img className={styles.title} src={value.img}></img>
-                                  <h4>{value.title}</h4> 
-                                  <h4><small>{value.position}</small></h4> 
-                                  {value.url ? <a href={value.url} target="_blank">{value.url.startsWith(isLinkedIn) ? <img className={styles.link} src={In}></img> : <img className={styles.link} src={other}></img>}</a> : '' }
+                                  <h4>{value.title}</h4>
+                                  <h4><small>{value.position}</small></h4>
+                                  {value.url ? <a href={value.url} target="_blank">{value.url.startsWith(isLinkedIn) ? <em className={styles.linkIn}></em> : <em className={styles.linkOther}></em>}</a> : '' }
                               </div>
                           );
                         })
