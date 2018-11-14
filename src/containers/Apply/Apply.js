d52533b0ddc6366a634d3dc5da6acdc2387cd8ad
liusheng112 <361707502@qq.com>
2018-11-14 16:53:50 +0800

    Optimization of style

--- src/containers/Apply/Apply.js
+++ src/containers/Apply/Apply.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet';
 
 class Apply extends Component {
   render() {
+    const styles = require('./Apply.scss');
     const iframeStyle = { height: '1000px', width: '900px', margin: '0 auto' };
     const iframeBox = { width: '100%', textAlign: 'center'};
 
--- src/containers/LabCareer/LabCareer.js
+++ src/containers/LabCareer/LabCareer.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet';
 
 class LabCareer extends Component {
   render() {
+    const styles = require('./LabCareer.scss');
     const iframeStyle = { height: '1000px', width: '900px', margin: '0 auto' };
     const iframeBox = { width: '100%', textAlign: 'center'};
 
