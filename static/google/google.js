9da00d3f8971fe8ac8c2739554f4b6198c4aa0f1
JasonHJ <hejian_best@163.com>
2019-01-25 12:13:10 +0800

    remove News tab

--- static/google/google.js
+++ static/google/google.js
@@ -1,5 +1,13 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
+// window.dataLayer = window.dataLayer || [];
+// function gtag() { dataLayer.push(arguments); }
+// gtag('js', new Date());
 
-gtag('config', 'UA-128074682-1');
+// gtag('config', 'UA-128074682-1');
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "UA-132143847-1");
