d8b5ef29b05cdaf8b8228013e5b27a71909deca6
JasonHJ <hejian_best@163.com>
2019-05-08 12:16:13 +0800

    update

--- src/containers/Crowdsale/Crowdsale.js
+++ src/containers/Crowdsale/Crowdsale.js
@@ -1,13 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { getNavButtonFunc } from 'redux/modules/auth';
 
+@connect(
+    state => ({language: state.auth.language, }),
+    {getNavButtonFunc}
+)
 class Crowdsale extends Component {
-    static propTypes = {};
+    static propTypes = {
+      language: PropTypes.string,
+    };
 
     render() {
       const styles = require('./Crowdsale.scss');
+      const {language} = this.props;
       return (
             <div className={styles.crowdsaleDiv}>
-              <h1>Coming soon...</h1>
+              <h1>{language === 'zn' ? 'Token sale已结束' : 'The token sale has been completed.'}</h1>
             </div>
       );
     }
