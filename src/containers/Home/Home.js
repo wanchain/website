28f122c4b2265dc85da538754d41451eae4f359d
JasonHJ <hejian_best@163.com>
2019-07-23 15:36:14 +0800

    update partner

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -250,6 +250,7 @@ export default class Home extends Component {
                           <a target="_blank" href="https://www.hashquark.io/#/supernode"><img src={hashquark} className={styles.img2}/></a>
                           <a target="_blank" href="https://snzholding.com"><img src={snz} className={styles.img4}/></a>
                           <a target="_blank" href="https://m.bepal.pro"><img src={bepal} className={styles.img2}/></a>
+                          <a target="_blank" href="https://www.rivex.io/"><img src={xdrive} className={styles.img6}/></a>
                           <p className={styles.posPartner}>More partners coming soon</p>
                         </div>
                     </div>
