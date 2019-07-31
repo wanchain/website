018f6dc983c085aa570ac886141f16e1383542d0
JasonHJ <hejian_best@163.com>
2019-07-31 11:51:13 +0800

    add partner

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -150,6 +150,7 @@ export default class Home extends Component {
     const snz = require('./image/snz.png');
     const bepal = require('./image/bepal.png');
     const xdrive = require('./image/xdrive.png');
+    const mango = require('./image/mango.png');
 
     const logo2 = require('./image/icoLoho2.png');
     const nav = require('./image/nav1.png');
@@ -250,7 +251,8 @@ export default class Home extends Component {
                           <a target="_blank" href="https://www.hashquark.io/#/supernode"><img src={hashquark} className={styles.img2}/></a>
                           <a target="_blank" href="https://snzholding.com"><img src={snz} className={styles.img4}/></a>
                           <a target="_blank" href="https://m.bepal.pro"><img src={bepal} className={styles.img2}/></a>
-                          <a target="_blank" href="https://www.rivex.io/"><img src={xdrive} className={styles.img6}/></a>
+                          <a target="_blank" href="https://www.rivex.io/"><img src={xdrive} className={styles.img7}/></a>
+                          <a target="_blank" href="http://mangoberry.io/"><img src={mango} className={styles.img2}/></a>
                           <p className={styles.posPartner}>More partners coming soon</p>
                         </div>
                     </div>
